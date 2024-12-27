1d8ff677107a42dc151bf7ec6a4e87c7b286ef8b
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-12-27 15:04:52 +0530

    Style: contact card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 export default function App(){
   return(
-    
     <article className="card">
       <section className="card__img-checkbox-section">
         <div className="card__checkbox-container">
@@ -12,25 +11,26 @@ export default function App(){
       </section>
       <section className="card__details-section">
         <div className="details-section__name-container">
-          <p>Mr. Tom</p>
+        <p className="title">Name:</p>
+          <p className="content">Mr. Tom</p>
         </div>
         <div className="details-section__mob-container">
-          <p>Mobile:</p>
-          <p>+91-9999222211</p>
+          <p className="title">Mobile:</p>
+          <p className="content" >+91-9999222211</p>
         </div>
         <div className="details-section__email-container">
-          <p>Email:</p>
-          <p>tom@gmail.com</p>
+          <p className="title">Email:</p>
+          <p className="content" >longestEmailIdPossible@gmail.com</p>
         </div>
       </section>
       <section className="card__button-section">
-        <button className="btn btn-call"><ion-icon name="call-outline"></ion-icon></button>
-        <button className="btn btn-text"><ion-icon name="send-outline"></ion-icon></button>
-        <button className="btn btn-whatsapp"><ion-icon name="logo-whatsapp"></ion-icon></button>
-        <button className="btn btn-mail"><ion-icon name="mail-outline"></ion-icon></button>
+        <button className="btn btn-call"><ion-icon className="icon" name="call-outline"></ion-icon></button>
+        <button className="btn btn-text"><ion-icon className="icon" name="send-outline"></ion-icon></button>
+        <button className="btn btn-whatsapp"><ion-icon className="icon" name="logo-whatsapp"></ion-icon></button>
+        <button className="btn btn-mail"><ion-icon className="icon" name="mail-outline"></ion-icon></button>
       </section>
       <section className="card__more-section">
-        <input type="button" value="More" />
+        <input type="button" value="More" className="more-section__btn"/>
       </section>
     </article>
     
